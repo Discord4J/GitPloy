9c8c61a882d28008b4c24ba16443f0954d8eb24f
austinv11 <austinv11@gmail.com>
2019-02-20 19:47:13 -0500

    Run gunicorn as a daemon

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 pip3 install -r requirements.txt
-gunicorn --bind 0.0.0.0:7777 --workers 1 --threads 1 app:app
+gunicorn --bind 0.0.0.0:7777 --workers 1 --threads 1 -D app:app
